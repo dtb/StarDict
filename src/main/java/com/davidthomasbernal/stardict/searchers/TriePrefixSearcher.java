7615655cca41410d78ccf51658585d1f7467c614
David Bernal <david.bernal@gmail.com>
2015-04-05 15:41:41 -0400

    Add trie backed searcher

---
+++ src/main/java/com/davidthomasbernal/stardict/searchers/TriePrefixSearcher.java
@@ -0,0 +1,34 @@
+package com.davidthomasbernal.stardict.searchers;
+
+import com.davidthomasbernal.stardict.Dictionary;
+import com.davidthomasbernal.stardict.util.datastructures.Trie;
+
+import java.util.List;
+
+/**
+ * Created by david on 4/5/15.
+ */
+public class TriePrefixSearcher {
+    protected final Dictionary dictionary;
+    private final Trie trie = new Trie();
+    private boolean trieInitialized = false;
+
+    public TriePrefixSearcher(Dictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public void ensureTrie() {
+        if (!trieInitialized) {
+            List<String> words = this.dictionary.getWords();
+            for (String word : words) {
+                trie.addWord(word);
+            }
+            trieInitialized = true;
+        }
+    }
+
+    public List<String> search(String prefix) {
+        ensureTrie();
+        return trie.prefixSearch(prefix);
+    }
+}
