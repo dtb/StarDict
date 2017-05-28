cc2abff7844f4bfc5e0a63291b8a74e07b1bde1a
vishvAsaH <vishvas.vasuki@gmail.com>
2017-05-28 16:31:09 -0700

    Add syn file support, project file, maven deployment file

---
+++ src/main/java/com/davidthomasbernal/stardict/parsers/SynParser.java
@@ -0,0 +1,68 @@
+package com.davidthomasbernal.stardict.parsers;
+
+import com.davidthomasbernal.stardict.dictionary.*;
+import com.davidthomasbernal.stardict.util.IndexInputStream;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SynParser {
+    private final DictionaryInfo dictionaryInfo;
+    private final DictionaryIndex dictionaryIndex;
+
+    public SynParser(DictionaryInfo info, DictionaryIndex index) {
+        this.dictionaryInfo = info;
+        this.dictionaryIndex = index;
+    }
+
+    public void parse(InputStream stream) {
+        IndexInputStream indexStream = new IndexInputStream(stream);
+
+        Set<IndexEntry> entries = readEntries(indexStream);
+        dictionaryIndex.addToIndex(entries);
+    }
+
+    protected Set<IndexEntry> readEntries(IndexInputStream indexStream) {
+        Set<IndexEntry> entries = new HashSet<>(dictionaryInfo.getWordCount());
+
+        boolean isPartial = false;
+        try {
+            //noinspection InfiniteLoopStatement
+            while (true) {
+                isPartial = false;
+
+                String word = indexStream.readWordString();
+
+                isPartial = true;
+
+                int originalWordIndex = indexStream.readInt();
+
+                IndexEntry index = dictionaryIndex.get(originalWordIndex);
+                index.words.add(word);
+                entries.add(index);
+
+                if (entries.size() > dictionaryInfo.getSynWordCount()) {
+                    throw new IndexFormatException("Found more words than specified in info.");
+                }
+            }
+        } catch (EOFException exception) {
+            // this is thrown when we reach the end of the file. If we're partway through initializing the entry,
+            // that's bad otherwise it's fine
+            if (isPartial) {
+                throw new IndexFormatException("Reached the end of the index before we finished parsing a word entry. The index is probably invalid.");
+            }
+        } catch (IOException exception) {
+            throw new IndexFormatException("IOException reading index", exception);
+        }
+        if (entries.size() != dictionaryInfo.getSynWordCount()) {
+            throw new IndexFormatException("Info and syn word counts did not match.");
+        }
+
+        return entries;
+    }
+}
